95fb793e4d5b100824b7bb6e5f5774f5931b55f2
unknown <narmada.vaddireddy97@gmail.com>
2023-04-14 16:13:41 +0530

    Added:Code to create address book array, add new contacts

--- AddressBook.js
+++ AddressBook.js
@@ -1,5 +1,6 @@
 const Contact = require("./Contact.js");
 const prompt = require('prompt-sync')();
+addressBook= new Array();
 try{
 let contact = new Contact();
 contact.first=prompt("Enter FirstName Capital and Minimum 3 Characters : "); 
@@ -10,6 +11,7 @@ contact.stateName = prompt("Enter State minimum 4 characters : ");
 contact.zipName = prompt("Enter Zip Six Digits: "); 
 contact.number = prompt("Enter Phone Number 10 Digits: "); 
 contact.emailId = prompt("Enter Email : ");
+addressBook.push(contact);
 console.log(contact.toString());
 } catch (e) {
     console.error(e);
